27e3d1421546f84f7b09893ab07ff4f914a0bc41
theeagle2407 <elijahoreoluwa45@gmail.com>
2026-04-18 19:28:59 +0100

    fix webpack config for optional deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@hyperlane-xyz/sdk': false,
+      '@hyperlane-xyz/registry': false,
+      '@hyperlane-xyz/utils': false,
+      '@fatsolutions/tongo-sdk': false,
+    };
+    return config;
+  },
+  experimental: {
+    optimizePackageImports: ['starkzap'],
+  },
 };
 
 export default nextConfig;
