c590022306686f46b3843aa32419b45218dc63d5
theeagle2407 <elijahoreoluwa45@gmail.com>
2026-04-18 19:49:03 +0100

    fix turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      '@hyperlane-xyz/sdk': false,
-      '@hyperlane-xyz/registry': false,
-      '@hyperlane-xyz/utils': false,
-      '@fatsolutions/tongo-sdk': false,
-    };
-    return config;
-  },
+  turbopack: {},
   experimental: {
     optimizePackageImports: ['starkzap'],
   },
